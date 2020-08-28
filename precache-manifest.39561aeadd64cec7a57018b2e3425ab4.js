8bf2abb92d2c9d6e10679ede502f26d1b0c0a683
Brendan Cariota <brendan@cariota.com>
2020-08-28 12:38:11 -0500

    Updates

---
+++ precache-manifest.39561aeadd64cec7a57018b2e3425ab4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6c167a1322d6b80271f0e649da20fd2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "69d3ec8d64771c28cd95",
+    "url": "/monsters-rolodex/static/css/main.92c9aca7.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69d3ec8d64771c28cd95",
+    "url": "/monsters-rolodex/static/js/main.e08be9db.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
